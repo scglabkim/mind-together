1e6288e9ad4f8e982c9f6e1de19fe00133cea925
scglabkim <167279028+scglabkim@users.noreply.github.com>
2026-09-01 12:53:47 +0900

    Connect Firebase realtime collaboration

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,11 @@
-// Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정에서 받은 값을 넣으세요.
 // 웹 Firebase 구성값은 비밀키가 아니며, 실제 접근 통제는 database.rules.json이 담당합니다.
-window.MIND_TOGETHER_FIREBASE_CONFIG = null;
+window.MIND_TOGETHER_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAMED5aeCWD1YUgbCgQONRul49hu806kBY",
+  authDomain: "mind-together.firebaseapp.com",
+  databaseURL: "https://mind-together-default-rtdb.firebaseio.com",
+  projectId: "mind-together",
+  storageBucket: "mind-together.firebasestorage.app",
+  messagingSenderId: "334326165033",
+  appId: "1:334326165033:web:5a42cc6418b97182d3b638",
+  measurementId: "G-3SKVS4RTH3"
+};
